a2a020fe901238623e7bae43841c39eabbd12dae
patriziorenelli <patriziore.pr@gmail.com>
2022-12-09 23:10:23 +0100

    Aggiunte le insert nel database

--- service/database/database.go
+++ service/database/database.go
@@ -69,6 +69,35 @@ func New(db *sql.DB) (AppDatabase, error) {
 		if err != nil {
 			return nil, fmt.Errorf("error creating database structure: %w", err)
 		}
+
+	    popola := `INSERT INTO user(id, username,description) VALUES (0000000, "marione_12", "sono mario di Roma");
+				   INSERT INTO user(id, username,description) VALUES (0000001, "luca_33", "sono Luchino");
+				   INSERT INTO user(id, username,description) VALUES (0000002, "Giorgia_Na", "Guardami negli occhi");
+				   INSERT INTO post(id, description, userId, photo) VALUES (0000000001, "la 1 foto",0000000, "LINK FOTO 1" );
+				   INSERT INTO post(id, description, userId, photo) VALUES (0000000002, "la 2 foto",0000000, "LINK FOTO 2" );
+				   INSERT INTO post(id, description, userId, photo) VALUES (0000000003, "la 3 foto",0000001, "LINK FOTO 3" );
+				   INSERT INTO post(id, description, userId, photo) VALUES (0000000004, "la 4 foto",0000000, "LINK FOTO 4" );
+				   INSERT INTO post(id, description, userId, photo) VALUES (0000000005, "la 5 foto",0000001, "LINK FOTO 5" );
+				   INSERT INTO ban(uid, uid2) VALUES (0000000,0000002 );
+				   INSERT INTO like(phid, uid) VALUES (0000000001,0000000);
+				   INSERT INTO like(phid, uid) VALUES (0000000001,0000002);
+				   INSERT INTO like(phid, uid) VALUES (0000000001,0000001);
+				   INSERT INTO like(phid, uid) VALUES (0000000004,0000001);
+				   INSERT INTO like(phid, uid) VALUES (0000000003,0000000);
+				   INSERT INTO like(phid, uid) VALUES (0000000003,0000001);
+				   INSERT INTO like(phid, uid) VALUES (0000000002,0000000);
+				   INSERT INTO like(phid, uid) VALUES (0000000002,0000001);
+				   INSERT INTO comment(phid, uid, cid, text) VALUES (0000000002,0000000, 0000001, "Primo commento");
+				   INSERT INTO comment(phid, uid, cid, text) VALUES (0000000002,0000001, 0000002, "Secondo commento");
+				   INSERT INTO comment(phid, uid, cid, text) VALUES (0000000003,0000000,0000003, "Terzo commento");
+				   INSERT INTO comment(phid, uid, cid, text) VALUES (0000000005,00000001,0000004, "Quarto commeto ");
+				   `
+		_, err = db.Exec(popola)
+		if err != nil {
+			return nil, fmt.Errorf("database population error: %w", err)
+		}
+
+
 	}
 
 	return &appdbimpl{
