817911be23605be416d09ab4f47f6ebd20b0c35b
patriziorenelli <patriziore.pr@gmail.com>
2022-12-11 15:12:27 +0100

    Modifica tipi degli id nell'api e agggiornamento database

--- service/database/database.go
+++ service/database/database.go
@@ -65,38 +65,38 @@ func New(db *sql.DB) (AppDatabase, error) {
 	if errors.Is(err, sql.ErrNoRows) {
 
 		// cambiare tutti gli id in integer per l'autoincrement 
-		sqlStmt := `CREATE TABLE user (id TEXT NOT NULL PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL);
-					CREATE TABLE post (id TEXT NOT NULL PRIMARY KEY AUTOINCREMENT,  description TEXT NOT NULL, uid TEXT NOT NULL, photo TEXT NOT NULL);
-					CREATE TABLE ban (uid TEXT NOT NULL, uid2 TEXT NOT NULL, PRIMARY KEY(uid, uid2), FOREIGN KEY(uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE NO ACTION, FOREIGN KEY(uid2) REFERENCES user(id) ON DELETE CASCADE ON UPDATE NO ACTION);
-					CREATE TABLE like (phid TEXT NOT NULL, uid TEXT NOT NULL, PRIMARY KEY(phid, uid), FOREIGN KEY(uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE NO ACTION, FOREIGN KEY(phid) REFERENCES post(id) ON DELETE CASCADE ON UPDATE NO ACTION);
-					CREATE TABLE comment (cid TEXT NOT NULL PRIMARY KEY AUTOINCREMENT, uid TEXT NOT NULL, phid TEXT NOT NULL, text TEXT NOT NULL,FOREIGN KEY(uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE NO ACTION, FOREIGN KEY(phid) REFERENCES post(id) ON DELETE CASCADE ON UPDATE NO ACTION );
+		sqlStmt := `CREATE TABLE user (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL);
+					CREATE TABLE post (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, uid INTEGER NOT NULL, photo TEXT NOT NULL);
+					CREATE TABLE ban (uid INTEGER NOT NULL, uid2 INTEGER NOT NULL, PRIMARY KEY(uid, uid2), FOREIGN KEY(uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE NO ACTION, FOREIGN KEY(uid2) REFERENCES user(id) ON DELETE CASCADE ON UPDATE NO ACTION);
+					CREATE TABLE like (phid INTEGER NOT NULL, uid INTEGER NOT NULL, PRIMARY KEY(phid, uid), FOREIGN KEY(uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE NO ACTION, FOREIGN KEY(phid) REFERENCES post(id) ON DELETE CASCADE ON UPDATE NO ACTION);
+					CREATE TABLE comment (cid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, uid INTEGER NOT NULL, phid INTEGER NOT NULL, text TEXT NOT NULL,FOREIGN KEY(uid) REFERENCES user(id) ON DELETE CASCADE ON UPDATE NO ACTION, FOREIGN KEY(phid) REFERENCES post(id) ON DELETE CASCADE ON UPDATE NO ACTION );
 		`
 		_, err = db.Exec(sqlStmt)
 		if err != nil {
 			return nil, fmt.Errorf("error creating database structure: %w", err)
 		}
 
-	    popola := `INSERT INTO user(id, username,description) VALUES ("0000000", "marione_12");
-				   INSERT INTO user(id, username,description) VALUES ("0000001", "luca_33");
-				   INSERT INTO user(id, username,description) VALUES ("0000002", "Giorgia_Na");
-				   INSERT INTO post(id, description, uid, photo) VALUES ("0000000001", "la 1 foto","0000000", "LINK FOTO 1" );
-				   INSERT INTO post(id, description, uid, photo) VALUES ("0000000002", "la 2 foto","0000000", "LINK FOTO 2" );
-				   INSERT INTO post(id, description, uid, photo) VALUES ("0000000003", "la 3 foto","0000001", "LINK FOTO 3" );
-				   INSERT INTO post(id, description, uid, photo) VALUES ("0000000004", "la 4 foto","0000000", "LINK FOTO 4" );
-				   INSERT INTO post(id, description, uid, photo) VALUES ("0000000005", "la 5 foto","0000001", "LINK FOTO 5" );
-				   INSERT INTO ban(uid, uid2) VALUES ("0000000","0000002" );
-				   INSERT INTO like(phid, uid) VALUES ("0000000001","0000000");
-				   INSERT INTO like(phid, uid) VALUES ("0000000001","0000002");
-				   INSERT INTO like(phid, uid) VALUES ("0000000001","0000001");
-				   INSERT INTO like(phid, uid) VALUES ("0000000004","0000001");
-				   INSERT INTO like(phid, uid) VALUES ("0000000003","0000000");
-				   INSERT INTO like(phid, uid) VALUES ("0000000003","0000001");
-				   INSERT INTO like(phid, uid) VALUES ("0000000002","0000000");
-				   INSERT INTO like(phid, uid) VALUES ("0000000002","0000001");
-				   INSERT INTO comment(phid, uid, cid, text) VALUES ("0000000002","0000000", "0000001", "Primo commento");
-				   INSERT INTO comment(phid, uid, cid, text) VALUES ("0000000002","0000001", "0000002", "Secondo commento");
-				   INSERT INTO comment(phid, uid, cid, text) VALUES ("0000000003","0000000","0000003", "Terzo commento");
-				   INSERT INTO comment(phid, uid, cid, text) VALUES ("0000000005","00000001","0000004", "Quarto commeto ");
+	    popola := `INSERT INTO user(id, username,description) VALUES (0000000, "marione_12");
+				   INSERT INTO user(id, username,description) VALUES (0000001, "luca_33");
+				   INSERT INTO user(id, username,description) VALUES (0000002, "Giorgia_Na");
+				   INSERT INTO post(id, description, uid, photo) VALUES (0000000001, 0000000, "LINK FOTO 1" );
+				   INSERT INTO post(id, description, uid, photo) VALUES (0000000002,0000000, "LINK FOTO 2" );
+				   INSERT INTO post(id, description, uid, photo) VALUES (0000000003, 0000001, "LINK FOTO 3" );
+				   INSERT INTO post(id, description, uid, photo) VALUES (0000000004, 0000000, "LINK FOTO 4" );
+				   INSERT INTO post(id, description, uid, photo) VALUES (0000000005, 0000001, "LINK FOTO 5" );
+				   INSERT INTO ban(uid, uid2) VALUES (0000000,0000002 );
+				   INSERT INTO like(phid, uid) VALUES (0000000001,0000000);
+				   INSERT INTO like(phid, uid) VALUES (0000000001,0000002);
+				   INSERT INTO like(phid, uid) VALUES (0000000001,0000001);
+				   INSERT INTO like(phid, uid) VALUES (0000000004,0000001);
+				   INSERT INTO like(phid, uid) VALUES (0000000003,0000000);
+				   INSERT INTO like(phid, uid) VALUES (0000000003,0000001);
+				   INSERT INTO like(phid, uid) VALUES (0000000002,0000000);
+				   INSERT INTO like(phid, uid) VALUES (0000000002,0000001);
+				   INSERT INTO comment(phid, uid, cid, text) VALUES (0000000002,0000000, 0000001, "Primo commento");
+				   INSERT INTO comment(phid, uid, cid, text) VALUES (0000000002,0000001, 0000002, "Secondo commento");
+				   INSERT INTO comment(phid, uid, cid, text) VALUES (0000000003,0000000,0000003, "Terzo commento");
+				   INSERT INTO comment(phid, uid, cid, text) VALUES (0000000005,00000001,0000004, "Quarto commeto ");
 				   `
 				   _, err = db.Exec(popola)
 				   if err != nil {
